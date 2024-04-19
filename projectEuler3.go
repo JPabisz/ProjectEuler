57a6803ee51f18045e42c2470ac0910fbf7a1660
James-gpg <jamespabisz@protonmail.com>
2024-04-19 00:45:07 -0400

    fixed stackoverflow error

--- projectEuler3.go
+++ projectEuler3.go
@@ -10,3 +10,12 @@ func main() {
 
 func largestPrime(a int)int {
     var b float64 = float64(a)
+    var x float64 = math.Sqrt(b)
+    var y int = int(x) - 1
+    for i:=y; i > 0; i-- {
+        if a%i == 0 && largestPrime(i) == 1 {
+            return i
+        }
+    }
+    return 1
+}
